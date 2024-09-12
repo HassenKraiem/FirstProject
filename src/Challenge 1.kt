dd5b81b5b4a6633e326032cf7842b7ab8af4a6fc
hassen <kraiemhassen26@gmail.com>
2024-09-12 11:22:54 +0100

    hi

---
+++ src/Challenge 1.kt
@@ -0,0 +1,25 @@
+fun main () {
+    val image = listOf(
+        listOf(1, 1, 0, 0, 0, 1),
+        listOf(1, 1, 0, 0, 0, 1),
+        listOf(1, 1, 1, 1, 1, 1),
+        listOf(1, 1, 1, 1, 1, 1)
+    )
+    println(findRectangle(image))
+
+
+}
+fun findRectangle1(list: List<List<Int>>):List<Int>{
+    val newList1= mutableListOf<List<Int>>()
+    val newList2=mutableListOf<Int>()
+    for (list1 in list){
+        if (0 in list1){
+            newList1.add(list1)
+            newList2.add(list.indexOfFirst { it===list1 })
+
+        }
+    }
+    return listOf(newList2[0],newList1[0].indexOf(0),newList2.last(),newList1.last().indexOfLast { it==0 })
+
+
+}
---
+++ src/challenge 1.0.kt
@@ -0,0 +1,32 @@
+fun main () {
+    val image1 = listOf(
+        listOf(1, 1, 0, 0, 0, 1),
+        listOf(1, 1, 0, 0, 0, 1),
+        listOf(1, 1, 1, 1, 1, 1),
+        listOf(1, 1, 1, 1, 1, 1)
+    )
+    val image2 = listOf(
+        listOf(1, 0, 0, 1, 1, 1),
+        listOf(1, 0, 0, 1, 1, 1),
+        listOf(1, 0, 0, 1, 1, 1))
+
+    val image3 = listOf(
+        listOf(1, 1, 1, 1, 1),
+        listOf(1, 1, 1, 0, 0),
+        listOf(1, 1, 1, 0, 0),
+        listOf(1, 1, 1, 0, 0),
+        listOf(1, 1, 1, 0, 0))
+    println(findRectangle(image1))
+    println(findRectangle(image2))
+    println(findRectangle(image3))
+
+
+
+}
+fun findRectangle(list: List<List<Int>>):List<Int>{
+    val list1= list.first { 0 in it }
+    val list2 =list.last { 0 in it  }
+
+    return listOf(list.indexOfFirst { 0 in it },list1.indexOf(0),list.indexOfLast { 0 in it },list2.lastIndexOf(0))
+
+}
